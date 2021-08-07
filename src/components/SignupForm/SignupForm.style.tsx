727f7211738efbeef64e29822fc9cec2e4b3d4fa
Muhammad Umair <hafizumairtariq1234@gmail.com>
2021-08-07 18:26:50 +0500

    updated

--- src/components/SignupForm/SignupForm.style.tsx
+++ src/components/SignupForm/SignupForm.style.tsx
@@ -2,6 +2,8 @@ import styled from "styled-components";
 
 export const Form = styled.form`
   display: flex;
-  margin-top: 1.5rem;
   flex-direction: column;
+  padding: 1.2rem;
+  flex-grow: 1;
+  justify-content: space-evenly;
 `;
--- src/components/SignupForm/SignupForm.tsx
+++ src/components/SignupForm/SignupForm.tsx
@@ -1,11 +1,34 @@
-import { TextField } from "@material-ui/core";
 import React, { FC } from "react";
+import {
+  Button,
+  Checkbox,
+  FormControlLabel,
+  TextField,
+} from "@material-ui/core";
 import { Form } from "./SignupForm.style";
 
 const SignupForm: FC = () => {
   return (
     <Form>
-      <TextField id="standard-basic" label="Standard" />
+      <TextField
+        id="outlined-basic"
+        label="First Name"
+        variant="outlined"
+        size="small"
+      />
+      <TextField
+        id="outlined-basic"
+        label="Last Name"
+        variant="outlined"
+        size="small"
+      />
+      <FormControlLabel
+        control={<Checkbox name="checkedB" color="primary" />}
+        label="I'am a millionaire"
+      />
+      <Button variant="contained" color="primary">
+        Next
+      </Button>
     </Form>
   );
 };
